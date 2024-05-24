a6cf2faa27ce939ecc0b4fab840f40ad7bb4dd9a
suresh522 <kajasuresh522@gmail.com>
2024-05-24 11:15:30 +0530

    changes done

--- sandeep_portfolio/sandeep_portfolio/jazzmin.py
+++ sandeep_portfolio/sandeep_portfolio/jazzmin.py
@@ -27,7 +27,7 @@ JAZZMIN_SETTINGS = {
     "welcome_sign": "Welcome to the sandeep ",
 
     # Copyright on the footer
-    "copyright": "sandeep ",
+    "copyright": "sandeep",
 
     # List of model admins to search from the search bar, search bar omitted if excluded
     # If you want to use a single search field you dont need to use a list, you can use a simple string 
